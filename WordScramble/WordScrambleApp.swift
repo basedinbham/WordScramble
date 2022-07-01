0f521b4fd1cba70a613a5fb835ba174400940be4
Kyle Warren <78629937+basedinbham@users.noreply.github.com>
2022-06-30 21:20:38 -0700

    first commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kyle Warren on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
